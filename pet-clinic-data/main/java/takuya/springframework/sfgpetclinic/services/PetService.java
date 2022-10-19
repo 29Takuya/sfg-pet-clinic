5a62e02584060e8873c00dbb0a1537f96088ba01
Takuya Shimada <gct3860@gmail.com>
2022-10-19 17:26:00 +0900

    refactoring to common crud interface. closes #10

--- pet-clinic-data/main/java/takuya/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/main/java/takuya/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package takuya.springframework.sfgpetclinic.services;
 
 import takuya.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
