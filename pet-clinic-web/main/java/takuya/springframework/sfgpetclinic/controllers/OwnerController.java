8716dfd67ffa0eec672f6a26c4a987cf600307a4
Takuya Shimada <gct3860@gmail.com>
2022-11-14 17:49:11 +0900

    adding Owners list and updated controller. Closes #16

--- pet-clinic-web/main/java/takuya/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/main/java/takuya/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package takuya.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import takuya.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
